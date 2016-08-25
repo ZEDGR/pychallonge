bfd859765a4d8112be2937e776b4f39aede05047
ZED <georlema@gmail.com>
2016-08-26 00:16:17 +0300

    more pypi configuration

--- challonge/__init__.py
+++ challonge/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "1.6.1"
-__author__ = "russ-"
+__version__ = "1.6.3"
+__author__ = "ZEDGR"
 
 from challonge import tournaments, matches, participants
 from challonge.api import set_credentials, get_credentials, fetch, ChallongeException
--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 import challonge
 
-
 setup(name = "pychal",
-      author = "ZEDGR",
-      author_email = "georlema@gmail.com",
-      url = "https://github.com/ZEDGR/pychal",
-      version = challonge.__version__,
-      keywords = ['tournaments', 'challonge'],
-      packages = [
-          'challonge',
-      ],
-      install_requires = [
-          'iso8601>=0.1.11',
-          'requests>=2.9.0',
-      ]
+    description = "Drop-in Replacement of pychallonge",
+    author = "ZEDGR",
+    author_email = "georlema@gmail.com",
+    url = "https://github.com/ZEDGR/pychal",
+    download_url = "https://github.com/ZEDGR/pychal/tarball/1.6.3",
+    license = "Public Domain",
+    version = challonge.__version__,
+    keywords = ['tournaments', 'challonge'],
+    packages = find_packages(),
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires = [
+        'iso8601',
+        'requests',
+    ]
 )
