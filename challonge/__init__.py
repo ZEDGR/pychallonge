2746955af70552b8a0debc4dd4594e8c0f1cd371
ZED <georlema@gmail.com>
2016-12-20 05:55:40 +0200

    create attachments module

--- challonge/__init__.py
+++ challonge/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "1.6.7"
 __author__ = "ZEDGR"
 
-from challonge import tournaments, matches, participants
+from challonge import tournaments, matches, participants, attachments
 from challonge.api import set_credentials, get_credentials, fetch, ChallongeException
