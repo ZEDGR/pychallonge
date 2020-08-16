b985f93ce592b73577f2b139cdc6fe71fb1a8e9f
George Zed <georlema@gmail.com>
2020-08-16 18:31:42 +0300

    fix and create docstrings for participants

--- challonge/participants.py
+++ challonge/participants.py
@@ -2,12 +2,30 @@ from challonge import api
 
 
 def index(tournament):
-    """Retrieve a tournament's participant list."""
-    return api.fetch_and_parse("GET", "tournaments/%s/participants" % tournament)
+    """Retrieve a tournament's participant list.
+
+    Args:
+        tournament (int or str): The tournament's id or name
+
+    Returns:
+        A list with the tournament's participants
+    """
+    return api.fetch_and_parse(
+        "GET",
+        "tournaments/%s/participants" % tournament)
 
 
 def create(tournament, name, **params):
-    """Add a participant to a tournament."""
+    """Add a participant to a tournament.
+
+    Args:
+        tournament (int or str): The tournament's id or name
+        name (str): The participant's name
+        **params (optional): extra keyword arguments used for the setup of the participant
+
+    Returns:
+        A dict representing the created participant
+    """
     params.update({"name": name})
 
     return api.fetch_and_parse(
@@ -18,13 +36,13 @@ def create(tournament, name, **params):
 def bulk_add(tournament, names, **params):
     """Bulk add participants to a tournament (up until it is started).
 
-    :param tournament: the tournament's name or id
-    :param names: the names of the participants
-    :type tournament: int or string
-    :type names: list or tuple
-    :return: each participants info
-    :rtype: a list of dictionaries
+    Args:
+        tournament (int or str): The tournament's id or name
+        names (list): A list of participants names (str)
+        **params (optional): extra keyword arguments used for the setup of the participants
 
+    Returns:
+        A list representing the created participants
     """
     params.update({"name": names})
 
@@ -34,14 +52,32 @@ def bulk_add(tournament, names, **params):
 
 
 def show(tournament, participant_id, **params):
-    """Retrieve a single participant record for a tournament."""
+    """Retrieve a single participant record for a tournament.
+
+    Args:
+        tournament (int or str): The tournament's id or name
+        participant_id (int): The participant's id for the specific tournament
+        **params (optional): The keywords arguments to include matches.
+
+    Returns:
+        A dict with the match details
+    """
     return api.fetch_and_parse(
         "GET", "tournaments/%s/participants/%s" % (tournament, participant_id), **params
     )
 
 
 def update(tournament, participant_id, **params):
-    """Update the attributes of a tournament participant."""
+    """Update the attributes of a tournament participant.
+
+    Args:
+        tournament (int or str): The tournament's id or name
+        participant_id (int): The participant's id for the specific tournament
+        **params (optional): The keywords arguments to include matches.
+
+    Returns:
+        None
+    """
     api.fetch(
         "PUT",
         "tournaments/%s/participants/%s" % (tournament, participant_id),
@@ -51,13 +87,33 @@ def update(tournament, participant_id, **params):
 
 
 def check_in(tournament, participant_id):
-    """Checks a participant in."""
-    api.fetch("POST", "tournaments/%s/participants/%s/check_in" % (tournament, participant_id))
+    """Checks a participant in.
+
+    Args:
+        tournament (int or str): The tournament's id or name
+        participant_id (int): The participant's id for the specific tournament
+
+    Returns:
+        None
+    """
+    api.fetch(
+        "POST",
+        "tournaments/%s/participants/%s/check_in" % (tournament, participant_id))
 
 
 def undo_check_in(tournament, participant_id):
-    """Marks a participant as having not checked in."""
-    api.fetch("POST", "tournaments/%s/participants/%s/undo_check_in" % (tournament, participant_id))
+    """Marks a participant as having not checked in.
+
+    Args:
+        tournament (int or str): The tournament's id or name
+        participant_id (int): The participant's id for the specific tournament
+
+    Returns:
+        None
+    """
+    api.fetch(
+        "POST",
+        "tournaments/%s/participants/%s/undo_check_in" % (tournament, participant_id))
 
 
 def destroy(tournament, participant_id):
@@ -69,6 +125,12 @@ def destroy(tournament, participant_id):
     If tournament is underway, mark a participant inactive, automatically
     forfeiting his/her remaining matches.
 
+    Args:
+        tournament (int or str): The tournament's id or name
+        participant_id (int): The participant's id for the specific tournament
+
+    Returns:
+        None
     """
     api.fetch("DELETE", "tournaments/%s/participants/%s" % (tournament, participant_id))
 
@@ -78,5 +140,11 @@ def randomize(tournament):
 
     Only applicable before a tournament has started.
 
+    Args:
+        tournament (int or str): The tournament's id or name
+        participant_id (int): The participant's id for the specific tournament
+
+    Returns:
+        None
     """
     api.fetch("POST", "tournaments/%s/participants/randomize" % tournament)
