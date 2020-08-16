7e030d8e3d8c2440ae48a7ac8ce6ca7be856adf9
George Zed <georlema@gmail.com>
2020-08-16 15:11:20 +0300

    fix var semantics

--- challonge/attachments.py
+++ challonge/attachments.py
@@ -1,40 +1,40 @@
 from challonge import api
 
 
-def index(tournament, match):
+def index(tournament, match_id):
     """Retrieve a set of attachments created for a specific match."""
     return api.fetch_and_parse(
         "GET",
-        "tournaments/%s/matches/%s/attachments" % (tournament, match))
+        "tournaments/%s/matches/%s/attachments" % (tournament, match_id))
 
 
-def create(tournament, match, **params):
+def create(tournament, match_id, **params):
     """Create a new attachment for the specific match."""
     return api.fetch_and_parse(
         "POST",
-        "tournaments/%s/matches/%s/attachments" % (tournament, match),
+        "tournaments/%s/matches/%s/attachments" % (tournament, match_id),
         "match_attachment",
         **params)
 
 
-def show(tournament, match, attachment):
+def show(tournament, match_id, attachment_id):
     """Retrieve a single match attachment record."""
     return api.fetch_and_parse(
         "GET",
-        "tournaments/%s/matches/%s/attachments/%s" % (tournament, match, attachment))
+        "tournaments/%s/matches/%s/attachments/%s" % (tournament, match_id, attachment_id))
 
 
-def update(tournament, match, attachment, **params):
+def update(tournament, match_id, attachment_id, **params):
     """Update the attributes of a match attachment."""
     api.fetch(
         "PUT",
-        "tournaments/%s/matches/%s/attachments/%s" % (tournament, match, attachment),
+        "tournaments/%s/matches/%s/attachments/%s" % (tournament, match_id, attachment_id),
         "match_attachment",
         **params)
 
 
-def destroy(tournament, match, attachment):
+def destroy(tournament, match_id, attachment_id):
     """Delete a match attachment."""
     api.fetch(
         "DELETE",
-        "tournaments/%s/matches/%s/attachments/%s" % (tournament, match, attachment))
+        "tournaments/%s/matches/%s/attachments/%s" % (tournament, match_id, attachment_id))
