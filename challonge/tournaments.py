2050cbca394184fa4112951a124524bfcfaaa52c
George Zed <georlema@gmail.com>
2020-08-16 18:31:04 +0300

    fix and create docstrings for tournaments

--- challonge/tournaments.py
+++ challonge/tournaments.py
@@ -2,50 +2,89 @@ from challonge import api
 
 
 def index(**params):
-    """Retrieve a set of tournaments created with your account."""
+    """Retrieve a set of tournaments created with your account.
+
+    Args:
+        **params (optional): the keyword arguments used to filter the results (state, type, created_after, created_before, subdomain)
+
+    Returns:
+        A list of dicts representing tournaments
+    """
     return api.fetch_and_parse("GET", "tournaments", **params)
 
 
 def create(name, url, tournament_type="single elimination", **params):
-    """Create a new tournament."""
-    params.update(
-        {
-            "name": name,
-            "url": url,
-            "tournament_type": tournament_type,
-        }
-    )
+    """Create a new tournament.
+
+    Args:
+        name (str): The name of the tournament
+        url (str): The name of the tournament for the URL subdomain/path
+        tournament_type (str, optional): The default is "single elimination". Other choices are double elimination, round robin, swiss
+        **params (optional): extra keyword arguments used for the setup of the tournament
+
+    Returns:
+        A dict representing the created tournament
+    """
+    params.update({
+        "name": name,
+        "url": url,
+        "tournament_type": tournament_type,
+    })
 
     return api.fetch_and_parse("POST", "tournaments", "tournament", **params)
 
 
 def show(tournament, **params):
-    """Retrieve a single tournament record created with your account."""
+    """Retrieve a single tournament record created with your account.
+
+    Args:
+        tournament (int or str): The tournament's id or name
+        **params (optional): The keywords arguments to include participants and/or matches
+
+    Returns:
+        A dict representing the tournament
+    """
     return api.fetch_and_parse("GET", "tournaments/%s" % tournament, **params)
 
 
 def update(tournament, **params):
-    """Update a tournament's attributes."""
+    """Update a tournament's attributes.
+
+    Args:
+        tournament (int or str): The tournament's id or name
+        **params (optional): extra keyword arguments used for the update of the tournament
+
+    Returns:
+        None
+    """
     api.fetch("PUT", "tournaments/%s" % tournament, "tournament", **params)
 
 
 def destroy(tournament):
-    """Deletes a tournament along with all its associated records.
+    """Deletes a tournament along with all its associated records. There is no undo, so use with care!
 
-    There is no undo, so use with care!
+    Args:
+        tournament (int or str): The tournament's id or name
 
+    Returns:
+        None
     """
     api.fetch("DELETE", "tournaments/%s" % tournament)
 
 
 def process_check_ins(tournament, **params):
-    """This should be invoked after a tournament's
-    check-in window closes before the tournament is started.
+    """This should be invoked after a tournament's check-in window closes before the tournament is started.
 
     1) Marks participants who have not checked in as inactive.
     2) Moves inactive participants to bottom seeds (ordered by original seed).
     3) Transitions the tournament state from 'checking_in' to 'checked_in'
 
+    Args:
+        tournament (int or str): The tournament's id or name
+        **params (optional): The keywords arguments to include participants and/or matches
+
+    Returns:
+        A dict representing the updated tournament
     """
     return api.fetch_and_parse("POST", "tournaments/%s/process_check_ins" % tournament, **params)
 
@@ -59,6 +98,12 @@ def abort_check_in(tournament, **params):
     1) Makes all participants active and clears their checked_in_at times.
     2) Transitions the tournament state from 'checking_in' or 'checked_in' to 'pending'
 
+    Args:
+        tournament (int or str): The tournament's id or name
+        **params (optional): The keywords arguments to include participants and/or matches
+
+    Returns:
+        A dict representing the updated tournament
     """
     return api.fetch_and_parse("POST", "tournaments/%s/abort_check_in" % tournament, **params)
 
@@ -69,23 +114,38 @@ def open_for_predictions(tournament, **params):
     Sets the state of the tournament to start accepting predictions.
     'prediction_method' must be set to 1 (exponential scoring) or 2 (linear scoring) to use this option.
 
+    Args:
+        tournament (int or str): The tournament's id or name
+        **params (optional): The keywords arguments to include participants and/or matches
+
+    Returns:
+        A dict representing the updated tournament
     """
     return api.fetch_and_parse("POST", "tournaments/%s/open_for_predictions" % tournament, **params)
 
 
 def start(tournament, **params):
-    """Start a tournament, opening up matches for score reporting.
+    """Start a tournament, opening up matches for score reporting. The tournament must have at least 2 participants.
 
-    The tournament must have at least 2 participants.
+    Args:
+        tournament (int or str): The tournament's id or name
+        **params (optional): The keywords arguments to include participants and/or matches
 
+    Returns:
+        A dict representing the updated tournament
     """
     return api.fetch_and_parse("POST", "tournaments/%s/start" % tournament, **params)
 
 
 def finalize(tournament, **params):
-    """Finalize a tournament that has had all match scores submitted,
-    rendering its results permanent.
+    """Finalize a tournament that has had all match scores submitted, rendering its results permanent.
 
+    Args:
+        tournament (int or str): The tournament's id or name
+        **params (optional): The keywords arguments to include participants and/or matches
+
+    Returns:
+        A dict representing the updated tournament
     """
     return api.fetch_and_parse("POST", "tournaments/%s/finalize" % tournament, **params)
 
@@ -96,5 +156,11 @@ def reset(tournament, **params):
     You can then add/remove/edit participants before starting the
     tournament again.
 
+    Args:
+        tournament (int or str): The tournament's id or name
+        **params (optional): The keywords arguments to include participants and/or matches
+
+    Returns:
+        A dict representing the updated tournament
     """
     return api.fetch_and_parse("POST", "tournaments/%s/reset" % tournament, **params)
